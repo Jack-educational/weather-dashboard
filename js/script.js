f3404aabef0384313bddbe21e24d75e5c2d47d14
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-02-03 19:36:41 +0000

    added function to parse json for todays weather and write the data to attributes to use in the HTML

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-// let it use jquery
 
-
+// define api key as a constant
 const apiKey = "b3930907c375f537a22fe584b3d8c57d"
+
+// store api key in localStorage for later use
+localStorage.setItem(apiKey, "b3930907c375f537a22fe584b3d8c57d");
+
+
 
 function getCoordinates (city) {
     let geocodingBaseURI = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey;
@@ -16,8 +20,8 @@ function getCoordinates (city) {
             lat = data[0].lat;
             lon = data[0].lon;
             getWeather(lat, lon);
-            console.log(lat);
-            console.log(lon);
+            // console.log(lat);
+            // console.log(lon);
         },
         error: function(error) {
             console.log(error);
@@ -32,14 +36,59 @@ function getWeather(lat, lon) {
         type: 'GET',
         dataType: 'json',
         success: function(data) {
-            forecastjson = data.list;
-            console.log(forecastjson);
+            var forecastjson = data;
+            // console.log(forecastjson);
+            displayWeather(forecastjson);
         },
         error: function(error) {
             console.log(error);
         }
-
     });
+}
+
+
+
+
+function displayWeather(forecastjson) {
+
+    // store todays weather data 
+
+    const weatherNow = [ 
+        {
+            city: forecastjson.city.name,
+            datetime: forecastjson.list[0].dt_txt,
+            icon: forecastjson.list[0].weather[0].icon,
+            temp: (forecastjson.list[0].main.temp)-273.15, // convert from kelvin to celsius
+            humidity: forecastjson.list[0].main.humidity,
+            wind: forecastjson.list[0].wind.speed
+        }
+
+    ]
+
+    // fetch the weather icon using the code from json data
+    var iconURL = "http://openweathermap.org/img/w/" + weatherNow[0].icon + ".png"; 
+    // setting src attribute for the weather icon image in the DOM
+    $('#icon-from-url').attr('src', iconURL);
+
+    // trim the date only from datetime string and wrap in parentheses
+    dateToday = "(" + weatherNow[0].datetime.substring(0, 10) + ")";
+
+    // set the src attributes for daily data in the DOM 
+    $('#city-name').text(weatherNow[0].city);
+    $('#current-date').text(dateToday);
+    $('#current-temp').text(weatherNow[0].temp.toFixed(2) + " °C");
+    $('#current-humidity').text(weatherNow[0].humidity + "%");
+    $('#current-wind').text(weatherNow[0].wind + " km/h");
+
+
+
+    // print the first entry of the array to the console
+    console.log("City name: " + weatherNow[0].city);
+    console.log("Date: " + weatherNow[0].datetime);
+    console.log("Weather Icon: " + weatherNow[0].icon);
+    console.log("Temperature: " + weatherNow[0].temp.toFixed(2) + " °C");
+    console.log("Humidity: " + weatherNow[0].humidity + "%");
+    console.log("Wind speed: " + weatherNow[0].wind + " km/h");
 }
 
 $(document).ready(function() {
@@ -50,4 +99,12 @@ $(document).ready(function() {
     });
 });
 
-// take the value of the text box and pass it to the getCoordinates function
+
+
+// console.log("City name: " + city);
+// console.log("Date: " + toString(forecastjson.list[0].dt_txt));
+// console.log("Weather Icon: " + (forecastjson.list[0].weather[0].icon));
+// console.log("Temperature :" + toString(forecastjson.list[0].main.temp));
+// console.log("Humidity: " + toString(forecastjson.list[0].main.humidity));
+// console.log("Wind speed: " + toString(forecastjson.list[0].wind.speed));
+
